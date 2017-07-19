c390d9f733cde2fb3571152b0e49135b63daa859
Robert Lummis <robert.lummis@gmail.com>
2017-07-19 15:44:55 -0400

    Gets all weight values post Jan 1, 2017 from HK database and displays them in a table view.

--- Weight Record/HealthKitHelper.swift
+++ Weight Record/HealthKitHelper.swift
@@ -10,21 +10,9 @@ import Foundation
 import HealthKit
 
 class HealthKitHelper {
-    var store = HKHealthStore()    // lazy var as per tutorial https://cocoacasts.com/managing-permissions-with-healthkit/
+    lazy var store = HKHealthStore()    // lazy var as per tutorial https://cocoacasts.com/managing-permissions-with-healthkit/
     var hkAccessRequestWasProcessed = false
     var viewController: WeightVC!
-    
-//    func authorizeHealthKitAccess() {
-//        if HKHealthStore.isHealthDataAvailable() {  // verify we're on a device and version that handles health kit
-//            let bodyMassToShare = Set( [HKQuantityType.quantityType(forIdentifier: .bodyMass)!] )
-//            let bodyMassToRead  = Set( [HKObjectType.quantityType(forIdentifier: .bodyMass)!] )
-//            store.requestAuthorization(toShare: bodyMassToShare, read: bodyMassToRead) {
-//                (success: Bool, error: Error?) in
-//                print("requestAuthorization returned; success: \(success)     error: \(error.debugDescription)")
-//                self.accessHealthDataBase()
-//            }
-//        }
-//    }
     
     func readWeightsAndDates(fromDate: Date, toDate: Date, vc: WeightVC) {
         viewController = vc
@@ -33,7 +21,7 @@ class HealthKitHelper {
         print("query using time zone: ", dateFormatter.timeZone.identifier)
         print("fromDate: \(fromDate),     toDate: \(toDate)")
         
-        if HKHealthStore.isHealthDataAvailable() {  // verify we're on a device and version that handles health kit
+        if HKHealthStore.isHealthDataAvailable() {  // only verifies we're on a device and version that implements health kit
             let bodyMassToShare = Set( [HKQuantityType.quantityType(forIdentifier: .bodyMass)!] )
             let bodyMassToRead  = Set( [HKObjectType.quantityType(forIdentifier: .bodyMass)!] )
             store.requestAuthorization(toShare: bodyMassToShare, read: bodyMassToRead) {
@@ -42,39 +30,6 @@ class HealthKitHelper {
                 self.accessHealthDataBase(fromDate: fromDate, toDate: toDate, vc: vc)
             }
         }
-//        authorizeHealthKitAccess()
-//        if !ok { fatalError("HealthKit access authorization failed") }
-//        
-//        guard let sampleType: HKSampleType = HKSampleType.quantityType(forIdentifier: .bodyMass) else {
-//            fatalError(" *** sampleType construction should never fail ***")
-//        }
-//        let predicate = HKQuery.predicateForSamples(withStart: fromDate, end: toDate, options: [])
-//        let query: HKSampleQuery = HKSampleQuery(sampleType: sampleType, predicate: predicate, limit: HKObjectQueryNoLimit, sortDescriptors: nil) {
-//            (theQuery, results, error) in
-//            if error != nil {
-//                print("error: ", error.debugDescription)
-//            } else {
-//                print("error is nil after query")
-//            }
-//            
-//            guard let samples = results as! [HKQuantitySample]? else {
-//                print("error: \(error.debugDescription)")
-//                fatalError("query failed in func 'getWeights': \(String(describing: error?.localizedDescription))" )
-//            }
-//            print("results.count: \(results!.count)   samples.count: \(samples.count)")
-//            
-//            for sample in samples {
-//                let pounds = sample.quantity.doubleValue(for: HKUnit.pound())
-//                let date = sample.startDate
-//                vc.weightsAndDates.append( (pounds, date) )
-//            }
-//            self.viewController.weightsAndDatesUpdated = true
-//            
-//        }
-//        // options arg [HKQueryOptions.strictStartDate, HKQueryOptions.strictEndDate]
-//
-//        store.execute(query)
-//        vc.updateUI()
     }
     
     func accessHealthDataBase(fromDate: Date, toDate: Date, vc: WeightVC) {
@@ -84,8 +39,10 @@ class HealthKitHelper {
             fatalError(" *** sampleType construction should never fail ***")
         }
         let predicate = HKQuery.predicateForSamples(withStart: fromDate, end: toDate, options: [])
+        
         let query: HKSampleQuery = HKSampleQuery(sampleType: sampleType, predicate: predicate, limit: HKObjectQueryNoLimit, sortDescriptors: nil) {
             (theQuery, results, error) in
+            print("in HKSampleQuery resultsHandler")
             if error != nil {
                 print("error: ", error.debugDescription)
             } else {
@@ -94,24 +51,22 @@ class HealthKitHelper {
 
             guard let samples = results as! [HKQuantitySample]? else {
                 print("error: \(error.debugDescription)")
-                fatalError("query failed in func 'getWeights': \(String(describing: error?.localizedDescription))" )
+                fatalError("query failed in func 'accessHealthDataBase': \(String(describing: error?.localizedDescription))" )
             }
-            print("results.count: \(results!.count)   samples.count: \(samples.count)")
+            print("query ran without causing fatal error")
+            print("query results.count: \(results!.count)   samples.count: \(samples.count)")
 
             for sample in samples {
                 let pounds = sample.quantity.doubleValue(for: HKUnit.pound())
                 let date = sample.startDate
                 vc.weightsAndDates.append( (pounds, date) )
             }
-            print("vc.weightsAndDates.count: \(vc.weightsAndDates.count)")
+            print("after query runs: vc.weightsAndDates.count: \(vc.weightsAndDates.count)")
             self.viewController.weightsAndDatesUpdated = true
             
         }
-        // options arg [HKQueryOptions.strictStartDate, HKQueryOptions.strictEndDate]
 
         store.execute(query)
-        vc.updateUI()
-        
     }
     
     func saveWeight(pounds: Double, note: String) {
--- Weight Record/WeightViewController.swift
+++ Weight Record/WeightViewController.swift
@@ -16,38 +16,31 @@ class WeightVC: UIViewController, UITableViewDataSource, UITableViewDelegate {
     let maxWeight = 250.0
     let helper = HealthKitHelper()
     var tableView: UITableView? = nil
-    lazy var weightsAndDates: [ (weight: Double, date: Date) ]  = []
+    var weightsAndDates: [ (weight: Double, date: Date) ]  = []
     var weightsAndDatesUpdated: Bool = false
-    let delay = 1.0 // wait for the HK query to run
-    
-    func updateUI() {
-//        getDataFromHealthKitDatabase()
-        print("start updateUI, delay: \(delay)")
-        perform(#selector(updateCells), with: nil, afterDelay: delay)
-    }
-    
-    func updateCells() {
-        print("start updateCells")
-        if tableView != nil && !weightsAndDates.isEmpty {
-            weightsAndDates.sort( by: {$0.date > $1.date} )
-            tableView!.reloadData()
-        } else {
-            fatalError("updateCells failed; either tableView is nil or weightsAndDates is empty")
-        }
-    }
+    let delay: TimeInterval = 1.0 // wait for the HK query to run; probably a shorter delay is OK
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        getDataFromHealthKitDatabase()
+        getDataFromHKDatabaseThenUpdateUIAfterDelay()
+        perform(#selector(updateCells), with: nil, afterDelay: delay)
     }
     
-    func getDataFromHealthKitDatabase() {
+    func getDataFromHKDatabaseThenUpdateUIAfterDelay() {
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "yyyy-MMM-dd HH:mm"
         let yearStart = dateFormatter.date(from: "2017-Jan-01 05:00")!
         let now = Date()
         helper.readWeightsAndDates(fromDate: yearStart, toDate: now, vc: self)
-        print("after helper returns \(weightsAndDates.count)")
+    }
+    
+    func updateCells() {
+        if tableView != nil && !weightsAndDates.isEmpty {
+            weightsAndDates.sort( by: {$0.date > $1.date} )
+            tableView!.reloadData()
+        } else {
+            fatalError("updateCells failed; tableView is nil or weightsAndDates is empty")
+        }
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
