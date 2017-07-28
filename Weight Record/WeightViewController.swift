7b8bfe0231ca5ee33e446c9f11d3034f1e0a9294
Robert Lummis <robert.lummis@gmail.com>
2017-07-27 22:52:15 -0400

    added ReadMe file and several other things, but all is not working smoothly yet

--- Weight Record/WeightViewController.swift
+++ Weight Record/WeightViewController.swift
@@ -9,16 +9,21 @@
 import UIKit
 import HealthKit
 
-class WeightVC: UIViewController, UITableViewDataSource, UITableViewDelegate, WeightAndDate {
+class WeightVC: UIViewController, UITableViewDataSource, UITableViewDelegate, WeightAndDate, UITextFieldDelegate {
 
     @IBOutlet weak var messageL: UILabel!
+    @IBOutlet weak var newWeightTF: UITextField!
     
     let hks = HKHealthStore()
-    let minWeight = 50.0
-    let maxWeight = 250.0
+    let minPounds = 40.0
+    let maxPounds = 399.0
     var helper: HealthKitHelper!
     var tableView: UITableView? = nil
+    var dateFormatter = DateFormatter()
+    var fromDate: Date!
+    var toDate: Date!
     var weightsAndDates: [ (weight: Double, date: Date) ]  = []
+    
     var messageText: String {
         get {
             return messageL.text ?? "messageText initial value"
@@ -28,21 +33,73 @@ class WeightVC: UIViewController, UITableViewDataSource, UITableViewDelegate, We
         }
     }
     
+    var newWeightText: String {
+        get {
+            return newWeightTF.text ?? ""
+        }
+        set {
+            if newValue == "" {
+                newWeightTF.text = "Enter new weight..."
+            } else {
+                newWeightTF.text = newValue
+            }
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        dateFormatter.dateFormat = "yyyy-MMM-dd HH:mm"
+        fromDate = dateFormatter.date(from: "2017-Jan-01 05:00")!   // start of this year
+        toDate = Date() // now
+        
         helper = HealthKitHelper(delegate: self)
+        newWeightTF.delegate = self
         messageText = ""
         
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "yyyy-MMM-dd HH:mm"
-        let yearStart = dateFormatter.date(from: "2017-Jan-01 05:00")!
-        let now = Date()
-        helper.getWeightsAndDates(fromDate: yearStart, toDate: now)
+        helper.getWeightsAndDates(fromDate: fromDate, toDate: toDate)
     }
     
     func displayMessage(msg: String) {
         messageL.text = msg
+    }
+    
+    func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
+        print("should begin editing")
+        return true
+    }
+    
+    @IBAction func clearEntryAction(_ sender: Any) {
+        newWeightTF.resignFirstResponder()
+        newWeightText = ""
+        
+    }
+    
+    @IBAction func saveWeightAction(_ sender: Any) {
+        print("save weight action")
+        
+        newWeightTF.resignFirstResponder()
+        if let wt = Double(newWeightTF.text!) {
+            if wt > minPounds && wt < maxPounds {
+                saveWeight(pounds: wt, note: "blah blah")
+                helper.getWeightsAndDates(fromDate: fromDate, toDate: toDate)
+                updateCells()
+            } else {
+                invalidWeight()
+            }
+        } else {
+            invalidWeight()
+        }
+        newWeightText = ""     // blank makes default text appear
+    }
+    
+    func invalidWeight() {
+        messageL.text = "valid weight range is \(minPounds) to \(maxPounds) pounds"
+        
+    }
+    
+    func saveWeight(pounds: Double, note: String) {
+        print("saveWeight func with pounds: \(pounds), note: \(note)")
     }
     
     func updateCells() {
@@ -52,7 +109,7 @@ class WeightVC: UIViewController, UITableViewDataSource, UITableViewDelegate, We
             fatalError("updateCells failed; tableView is nil or weightsAndDates is empty")
         }
     }
-
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "weightAndDateCell", for: indexPath) as! WeightAndDateCell
         cell.updateFields(withSample: weightsAndDates[indexPath.row])
