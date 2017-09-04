57c23042be5b17516e0d90675ba57a07771dd9c4
Robert Lummis <robert.lummis@gmail.com>
2017-09-03 23:26:48 -0400

    Add SettingsVC to Main.storyboard. Make modal segue with ID "WeightVC-SettingsVC". At this point the SettingsVC is blank.

---
+++ SettingsVC.swift
@@ -0,0 +1,14 @@
+//
+//  SettingsVC.swift
+//  Weight Record
+//
+//  Created by Robert Lummis on 9/3/17.
+//  Copyright © 2017 ElectricTurkeySoftware. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SettingsVC : UIViewController {
+
+}
