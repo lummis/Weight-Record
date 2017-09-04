8107d7560e601f0f3a02246e8537efc60f0c2e86
Robert Lummis <robert.lummis@gmail.com>
2017-09-03 23:39:29 -0400

    In SettingsVC added Done button that returns to the homeVC. Also added viewDidLoad and viewDidAppear.

--- SettingsVC.swift
+++ SettingsVC.swift
@@ -10,5 +10,22 @@ import Foundation
 import UIKit
 
 class SettingsVC : UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("viewDidLoad")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        print("viewDidAppear")
+    }
+    
+    @IBAction func doneVAction(_ sender: Any) {
+        
+        dismiss(animated: true, completion: nil)
+    }
 
 }
